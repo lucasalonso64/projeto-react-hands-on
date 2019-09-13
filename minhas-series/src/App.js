9400e733ec602632121a9fba53c3f6de31be0575
Lucas Alonso <alonsosistemas@gmail.com>
2019-09-12 22:25:07 -0300

    v01

---
+++ minhas-series/src/App.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react'
+import Header from './Header'
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom'
+
+const Home = () => {
+  return <h1>Home</h1>
+}
+
+const Generos = () => {
+  return <h1>Generos</h1>
+}
+
+
+
+
+function App() {
+
+  return (
+    <Router>
+      <div>
+      <Header />
+      <Route path='/' exact component={Home} />
+      <Route path='/generos' component={Generos} />
+    </div>
+
+    </Router>
+    
+  );
+}
+
+export default App;
