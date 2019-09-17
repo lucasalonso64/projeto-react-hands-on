c3a1573a8fc2dff1cb63565db1080c41e7ec6943
Lucas <alonsosistemas@gmail.com>
2019-09-17 14:52:45 -0300

    v01

--- minhas-series/src/App.js
+++ minhas-series/src/App.js
@@ -1,8 +1,11 @@
-import React, { useState } from 'react'
-import Header from './Header'
+import React, {useState, useEffect } from 'react';
+import Header from '/Header'
+import axios from 'axios'
+
 import {
   BrowserRouter as Router,
   Route
+
 } from 'react-router-dom'
 
 const Home = () => {
@@ -12,23 +15,28 @@ const Home = () => {
 const Generos = () => {
   return <h1>Generos</h1>
 }
-
-
-
-
 function App() {
+  const [data, setData] = useState({})
+  useEffect(() => {
+    axios.get('/api').then(res => {
+      setData(res.data)
 
+    })
+   
+  }, [])
   return (
     <Router>
       <div>
       <Header />
-      <Route path='/' exact component={Home} />
-      <Route path='/generos' component={Generos} />
+      <Route path="/" exact component={Home} />
+      <Route path="/generos" component={Generos} />
+      <pre>{JSON.stringify(data)}</pre>
     </div>
-
     </Router>
     
   );
+
 }
+
 
 export default App;
--- minhas-series/src/Header.js
+++ minhas-series/src/Header.js
@@ -1,35 +1,40 @@
 import React, { useState } from 'react';
-
 import {
     Navbar,
+    NavbarBrand,
     Nav,
+    Collapse,
     NavItem,
     NavLink,
-    NavbarBrand,
-    Collapse,
     NavbarToggler
-  } from 'reactstrap'
-  import { Link } from 'react-router-dom'
+  }
+    from 'reactstrap'
+
+import { Link } from 'react-router-dom'
   
   const Header = () => {
     const [open, setOpen] = useState(false)
     const toggle = () => {
       setOpen(!open)
-  
     }
-  return (
-    <Navbar color="light" light expand="md">
-          <NavbarBrand tag={Link} to="/">Minhas Séries</NavbarBrand>
-          <NavbarToggler onClick={toggle} />
-          <Collapse isOpen={open} navbar>
-            <Nav className='ml-auto' navbar>
+    return (
+      <Navbar color="ligth" light expand="md">
+        <NavbarBrand tag={Link} to="/">Minhas Séries</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={open} navbar>
+          <Nav className="ml-auto" navbar>
             <NavItem>
-                <NavLink tag={Link} to="/generos">Genêros</NavLink>
-              </NavItem>
-            </Nav>            
-          </Collapse>
-        </Navbar>
-  )
+              <NavLink tag={Link} to="/generos">Gêneros</NavLink>
+            </NavItem>
+  
+          </Nav>
+  
+        </Collapse>
+  
+      </Navbar>
+  
+    )
+  
   }
 
   export default Header;
